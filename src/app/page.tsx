f5ebca0f661674e23d0fb6b8886aa98df83af637
Jhonny Briones <brionesjohnny43@gmail.com>
2024-01-04 17:42:31 -0600

    simple header

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,20 @@
 import React from "react";
 import { NextPage } from "next";
 
+const Header: React.FC = () => {
+  return (
+    <header style={{ background: "#333", color: "#fff", padding: "1rem" }}>
+      <h1>My Simple Header</h1>
+    </header>
+  );
+};
+
 const App: NextPage = () => {
   return (
     <div>
-      <h1>hola mundo</h1>
+      <Header />
+      <h1>Johnny Briones</h1>
+      {/* Resto de tu contenido */}
     </div>
   );
 };
