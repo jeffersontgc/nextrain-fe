417bbe4a952f2a1de017b7e6ac5ff38d780e1ff9
Cinthia <brionesirigoyenc@gmail.com>
2024-01-30 09:39:42 -0600

    connection with the backend

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,32 +3,44 @@ import React from "react";
 import HeaderHome from "@/components/header";
 import ModalForm from "@/components/utils/modal";
 import ProductCreateForm from "@/components/products/forms/create";
+import { categorys } from "../../../nextrain-be/src/database/seader/categorys";
 
 const HomePage = () => {
   const [openModal, setOpenModal] = React.useState<boolean>(false);
+  const [selectCategory, setSelectCategory] = React.useState<string>("");
   const [formData, setFormData] = React.useState<any>({
     name: "",
     url: "",
     price: "",
+    category_uuid: "",
     description: "",
   });
 
-  console.log(formData);
-
-  const handleSubmit = async () => {};
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+    const payload = {
+      ...formData,
+      category_uuid: selectCategory,
+    };
+    console.log("PAYLOAD TO BACKEND", payload);
+    setOpenModal(false);
+  };
 
   return (
     <div>
       <HeaderHome />
+      <button onClick={() => setOpenModal(!openModal)}>Click</button>
       <ModalForm
         title="Crear Producto"
-        visible={!openModal}
+        visible={openModal}
         handleVisible={() => setOpenModal(!openModal)}
       >
         <ProductCreateForm
           handleSubmit={handleSubmit}
           formData={formData}
+          setSelectCategory={setSelectCategory}
           setFormData={setFormData}
+          selectCategory={selectCategory}
         />
       </ModalForm>
     </div>
